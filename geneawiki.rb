ac4894d4c0888988e03b239acc5a978a18cf44b4
Xavier Riley <xavriley@hotmail.com>
2014-08-05 15:47:12 +0100

    Update memcachier connection details

--- geneawiki.rb
+++ geneawiki.rb
@@ -4,11 +4,17 @@ require 'sinatra'
 require 'rack/cache'
 require 'dalli'
 
-if memcache_servers = ENV["MEMCACHE_SERVERS"] || ENV["MEMCACHIER_SERVERS"]
+if memcache_servers = ENV["MEMCACHE_SERVERS"] 
   use Rack::Cache,
     verbose: true,
     metastore:   "memcached://#{memcache_servers}",
     entitystore: "memcached://#{memcache_servers}"
+elsif memcachier_servers = ENV["MEMCACHIER_SERVERS"]
+  cache = Dalli::Client.new memcachier_servers.split(','), {
+    username: ENV['MEMCACHIER_USERNAME'],
+    password: ENV['MEMCACHIER_PASSWORD']
+  }
+  use Rack::Cache, verbose: true, metastore: cache, entitystore: cache
 end
 use Rack::Deflater
 set :static_cache_control, [:public, max_age: 60 * 60 * 24 * 365]
